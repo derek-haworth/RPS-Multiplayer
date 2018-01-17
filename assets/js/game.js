f13fc6fa445bf6c56251bf6e6832e4084b339d57
Derek Haworth <derek_haworth@bradycorp.com>
2018-01-17 16:58:19 -0600

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAP9t4t0AfSkll898LtXw4RJGKalbULA6k",
+  authDomain: "rock-paper-scissors-e0439.firebaseapp.com",
+  databaseURL: "https://rock-paper-scissors-e0439.firebaseio.com",
+  projectId: "rock-paper-scissors-e0439",
+  storageBucket: "rock-paper-scissors-e0439.appspot.com",
+  messagingSenderId: "362071363983"
+};
+
+firebase.initializeApp(config);
