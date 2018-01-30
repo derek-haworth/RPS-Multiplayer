89a7d72794f0056057c5f21f38b3cb7d60fd1796
derek-haworth <derekhaw1@gmail.com>
2018-01-30 17:24:18 -0600

    set up firebase and initial vars

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,32 @@
+$(document).ready(function() {
+	
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAP9t4t0AfSkll898LtXw4RJGKalbULA6k",
-  authDomain: "rock-paper-scissors-e0439.firebaseapp.com",
-  databaseURL: "https://rock-paper-scissors-e0439.firebaseio.com",
-  projectId: "rock-paper-scissors-e0439",
-  storageBucket: "rock-paper-scissors-e0439.appspot.com",
-  messagingSenderId: "362071363983"
-};
+	var config = {
+	  apiKey: "AIzaSyAP9t4t0AfSkll898LtXw4RJGKalbULA6k",
+	  authDomain: "rock-paper-scissors-e0439.firebaseapp.com",
+	  databaseURL: "https://rock-paper-scissors-e0439.firebaseio.com",
+	  projectId: "rock-paper-scissors-e0439",
+	  storageBucket: "rock-paper-scissors-e0439.appspot.com",
+	  messagingSenderId: "362071363983"
+	};
+	
+	firebase.initializeApp(config);
+	
+	function initGame() {
+		var database = firebase.database();
+	
+		//variables
+		var name = '';
+			wins = 0,
+			losses = 0,
+			choice = ' ',
+			playerNumber = 0,
+			turns = 0,
 
-firebase.initializeApp(config);
+	}
 
-var database = firebase.database();
+	intiGame();
+
+
+});
+
